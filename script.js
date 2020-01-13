33f84b940d4f1c273c52c106e69eccae5ecb72d1
Jeff Johnson <john1209@algonquinlive.com>
2020-01-12 23:34:06 -0500

    created index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+let name = 'Jeff Johnson'
+let number = Math.random()
+
+console.log('Hello, my name is ' + name)
+let newNumber = number * 2
+newNumber = newNumber * 5
+newNumber = newNumber / number - 7
